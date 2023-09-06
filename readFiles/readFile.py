cabe28deeb8561b9a9d01146bd6b4dc5f3bbff84
Viktor Kindrat <kindratvictor07@gmail.com>
2023-09-06 17:00:59 +0300

    PEP 8 issues fixes

--- readFiles/readFile.py
+++ readFiles/readFile.py
@@ -1,14 +1,19 @@
 import string
 
-with open("files/lorem.txt", "r", encoding="utf-8") as text_file: 
-    trans = str.maketrans("", "", string.punctuation)
-    wodrList = text_file.read().translate(trans).split(" ")
 
-    res = {}
-    for el in wodrList:
-        if (el in res): 
-            res[el] += 1
-        else: 
-            res[el] = 1
+def word_analise(path):
+    with open(path, "r", encoding="utf-8") as text_file:
+        trans = str.maketrans("", "", string.punctuation)
+        word_list = text_file.read().translate(trans).split(" ")
 
-    print("\n\nWord analisys", res, sep=": \n")
+        res = {}
+        for el in word_list:
+            if el in res:
+                res[el] += 1
+            else:
+                res[el] = 1
+
+        return res
+
+
+print(word_analise("files/lorem.txt"))
